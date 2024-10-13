85b96bc0ccf0394b788ce98744bcfe5f322c86f7
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-10-14 03:30:59 +0500

    feat: template authOptions

---
+++ lib/auth.ts
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import User from '../models/user'
+
+export const authOptions = {
+	providers: [
+		Credentials({
+			credentials: {
+				email: { label: 'Email', type: 'email' },
+				password: { label: 'Password', type: 'password' }
+			},
+			authorize: async credentials => {
+				const user = await User.findOne({ email: credentials?.email })
+
+				if (user) {
+					const isValidPassword = user.password === credentials?.password
+					if (isValidPassword) {
+						return user
+					}
+				}
+				return null
+			}
+		})
+	]
+}
+
+export const { handlers, auth, signIn, signOut } = NextAuth(authOptions)
