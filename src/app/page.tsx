046de1da85acb4713bed019acef9137e0e1c4a42
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-10-14 03:31:13 +0500

    feat: info about auth user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function App() {
+import { auth } from '@@/lib/auth'
+
+export default async function App() {
+	const session = await auth()
+
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			App
+		<main className='flex min-h-screen items-center justify-center p-24'>
+			<div className='text-center'>
+				<h1 className='text-3xl font-bold mb-6'>Welcome to the App</h1>
+				{session && session.user ? (
+					<div>
+						<p className='text-lg'>Signed in as {session.user?.email}</p>
+						<p className='text-lg'>Name: {session.user?.name}</p>
+					</div>
+				) : (
+					<p className='text-lg'>You are not signed in</p>
+				)}
+			</div>
 		</main>
 	)
 }
