c887621ccbf7328fe2a61bd41b3bf4cf33e1a35b
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-10-14 09:56:43 +0500

    asfasf

--- components/TelegramWidget.tsx
+++ components/TelegramWidget.tsx
@@ -1,31 +1,25 @@
 'use client'
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import Image from 'next/image'
 
-import { signIn } from 'next-auth/react'
+import React, { useEffect } from 'react'
 
 const TelegramWidget = () => {
-	const handleGoogleSignIn = async () => {
-		await signIn('google', { callbackUrl: '/' })
-	}
-	return (
-		<Button
-			onClick={handleGoogleSignIn}
-			variant='outline'
-			size='lg'
-			className='flex items-center justify-between gap-2'
-		>
-			<Image
-				src='/google-logo.svg'
-				alt='Google logo'
-				width={24}
-				height={24}
-				className='mr-3'
-			/>
-			<p className='ml-2'>Telegram</p>
-		</Button>
-	)
+	useEffect(() => {
+		const script = document.createElement('script')
+		script.src = 'https://telegram.org/js/telegram-widget.js?22'
+		script.async = true
+		script.setAttribute('data-telegram-login', 'NextTelegramAuth_bot')
+		script.setAttribute('data-size', 'large')
+		script.setAttribute('data-userpic', 'false')
+		script.setAttribute(
+			'data-auth-url',
+			`${process.env.NEXT_PUBLIC_APP_URL}/api/auth/telegram/callback`
+		)
+		script.setAttribute('data-request-access', 'write')
+		script.setAttribute('data-callback', '/')
+		document.getElementById('telegram-login-btn')?.appendChild(script)
+	}, [])
+
+	return <div id='telegram-login-btn'></div>
 }
 
 export default TelegramWidget
