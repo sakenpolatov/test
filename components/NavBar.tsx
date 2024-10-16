a9d4beb3bcc22066a04f600690a866c5fa616de1
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-10-16 23:33:45 +0500

    feat: navbar on header

---
+++ components/NavBar.tsx
@@ -0,0 +1,47 @@
+import LogoutButton from '@@/components/LogoutButton'
+import { auth } from '@@/lib/auth'
+import { FaUserTie } from 'react-icons/fa'
+
+export default async function NavBar() {
+	const session = await auth()
+
+	if (!session || !session.user) return null
+
+	return (
+		<nav className='bg-gray-800 p-4 flex justify-between items-center h-16'>
+			<ul className='flex space-x-6'>
+				<li>
+					<a
+						href='/'
+						className='text-white hover:text-gray-300 transition-colors duration-200'
+					>
+						Home
+					</a>
+				</li>
+				<li>
+					<a
+						href='/map'
+						className='text-white hover:text-gray-300 transition-colors duration-200'
+					>
+						Map
+					</a>
+				</li>
+				<li>
+					<a
+						href='/info'
+						className='text-white hover:text-gray-300 transition-colors duration-200'
+					>
+						Info
+					</a>
+				</li>
+			</ul>
+			<div className='flex items-center space-x-4'>
+				<div className='flex items-center space-x-2'>
+					<FaUserTie />
+					<span className='text-white'>{session.user.name || 'Guest'}</span>
+				</div>
+				<LogoutButton />
+			</div>
+		</nav>
+	)
+}
