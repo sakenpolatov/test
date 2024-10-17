a27e866ce7bb3c73b446f84377ceed0789f1f552
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-10-17 05:20:56 +0500

    feat: add table shadcn-ui

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,18 +1,22 @@
-import FormMark from '@/components/FormMark'
-import YandexMap from '@/components/YandexMap'
 import React from 'react'
+import FormMark from './ui/FormMark'
+import YandexMap from './ui/YandexMap'
+import TableMarks from './ui/TableMarks'
 
 const MapPage = () => {
 	return (
-		<main className='flex h-full items-center justify-center p-24'>
-			<div className='flex w-full justify-center space-x-6'>
+		<main className='flex flex-col items-center justify-center p-24 space-y-6'>
+			<div className='flex w-full justify-between space-x-6'>
 				<div className='w-1/2'>
 					<FormMark />
 				</div>
 
-				<div className='w-1/2  flex items-center justify-center'>
+				<div className='w-1/2'>
 					<YandexMap />
 				</div>
+			</div>
+			<div className='w-full flex justify-center'>
+				<TableMarks />
 			</div>
 		</main>
 	)
