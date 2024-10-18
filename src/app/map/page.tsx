c130c2fc8fafc9dc2c66e446732ae5f6aee6481e
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-10-18 14:57:43 +0500

    refactor: del usesession

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,9 +1,23 @@
+'use client'
+
 import React from 'react'
 import FormMark from './ui/FormMark'
 import YandexMap from './ui/YandexMap'
 import TableMarks from './ui/TableMarks'
+import { useSession } from 'next-auth/react'
+import Unlogged from './ui/Unlogged'
 
 const MapPage = () => {
+	const { data: session, status } = useSession()
+
+	if (status === 'loading') {
+		return <p>Loading...</p>
+	}
+
+	if (!session) {
+		return <Unlogged />
+	}
+
 	return (
 		<main className='flex flex-col items-center justify-center p-24 space-y-6'>
 			<div className='flex w-full justify-between space-x-6'>
