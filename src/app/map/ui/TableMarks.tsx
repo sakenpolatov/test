78efa5f86fe8f00eca2b1a19c4a167b009f7cac4
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-10-24 23:23:03 +0500

    style: add loader

--- src/app/map/ui/TableMarks.tsx
+++ src/app/map/ui/TableMarks.tsx
@@ -20,7 +20,6 @@ import {
 	SelectContent,
 	SelectItem
 } from '@/components/ui/select'
-
 import {
 	Pagination,
 	PaginationContent,
@@ -32,10 +31,12 @@ import {
 import { usePagination } from '@/hooks/usePagination'
 import { initialItemsPerPage } from '@/constants/variables'
 import { anchorToMap } from '@/utils/anchorToMap'
+import Loader from '@@/components/Loader/loader'
 
 const TableMarks = memo(() => {
 	const dispatch = useAppDispatch()
 	const markers = useAppSelector(state => state.marks.markers)
+	const loading = useAppSelector(state => state.marks.loading)
 
 	const {
 		currentPage,
@@ -62,102 +63,103 @@ const TableMarks = memo(() => {
 
 	return (
 		<>
-			{markers.length === 0 ? (
-				<NoMarkers />
-			) : (
-				<div className='w-full overflow-x-auto max-w-4xl mx-auto border-2 border-gray-500 shadow-lg rounded-md'>
-					<Table className='min-w-full'>
-						<TableHeader>
-							<TableRow>
-								<TableHead className='bg-gray-500 text-gray-700 w-1/12 border border-black text-center'>
-									Тип
-								</TableHead>
-								<TableHead className='bg-gray-500 text-gray-700 w-4/12 border border-black text-center'>
-									Локация
-								</TableHead>
-								<TableHead className='bg-gray-500 text-gray-700 w-3/12 border border-black text-center'>
-									Источник информации
-								</TableHead>
-								<TableHead className='bg-gray-500 text-gray-700 w-3/12 border border-black text-center'>
-									Комментарии
-								</TableHead>
-								<TableHead className='bg-gray-500 text-gray-700 w-4/12 border border-black text-center'>
-									Координаты
-								</TableHead>
-								<TableHead className='bg-gray-500 text-gray-700 w-4/12 border border-black text-center'>
-									<Select onValueChange={handleItemsPerPageChange}>
-										<SelectTrigger className='w-[100px] text-black'>
-											<SelectValue placeholder={initialItemsPerPage} />
-										</SelectTrigger>
-										<SelectContent>
-											<SelectItem value='5'>5</SelectItem>
-											<SelectItem value='10'>10</SelectItem>
-											<SelectItem value='20'>20</SelectItem>
-											<SelectItem value='50'>50</SelectItem>
-										</SelectContent>
-									</Select>
-								</TableHead>
+			<div className='relative w-full overflow-x-auto max-w-4xl mx-auto border-2 border-gray-500 shadow-lg rounded-md'>
+				{loading && (
+					<div className='absolute inset-0 flex justify-center items-center'>
+						<Loader />
+					</div>
+				)}
+				<Table className='min-w-full'>
+					<TableHeader>
+						<TableRow>
+							<TableHead className='bg-gray-500 text-gray-700 w-1/12 border border-black text-center'>
+								Тип
+							</TableHead>
+							<TableHead className='bg-gray-500 text-gray-700 w-4/12 border border-black text-center'>
+								Локация
+							</TableHead>
+							<TableHead className='bg-gray-500 text-gray-700 w-3/12 border border-black text-center'>
+								Источник информации
+							</TableHead>
+							<TableHead className='bg-gray-500 text-gray-700 w-3/12 border border-black text-center'>
+								Комментарии
+							</TableHead>
+							<TableHead className='bg-gray-500 text-gray-700 w-4/12 border border-black text-center'>
+								Координаты
+							</TableHead>
+							<TableHead className='bg-gray-500 text-gray-700 w-4/12 border border-black text-center'>
+								<Select onValueChange={handleItemsPerPageChange}>
+									<SelectTrigger className='w-[100px] text-black'>
+										<SelectValue placeholder={initialItemsPerPage} />
+									</SelectTrigger>
+									<SelectContent>
+										<SelectItem value='5'>5</SelectItem>
+										<SelectItem value='10'>10</SelectItem>
+										<SelectItem value='20'>20</SelectItem>
+										<SelectItem value='50'>50</SelectItem>
+									</SelectContent>
+								</Select>
+							</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{visibleItems.map(item => (
+							<TableRow
+								key={item._id}
+								className='hover:bg-transparent cursor-pointer'
+								onClick={() => handleRowClick(item.coordinates)}
+							>
+								<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
+									{item.type}
+								</TableCell>
+								<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
+									{item.address}
+								</TableCell>
+								<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
+									{item.label}
+								</TableCell>
+								<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
+									{item.description}
+								</TableCell>
+								<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
+									{item.coordinates
+										? `${item.coordinates.latitude}, ${item.coordinates.longitude}`
+										: 'Не указаны'}
+								</TableCell>
+								<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
+									<button
+										onClick={e => {
+											e.stopPropagation()
+											dispatch(deleteMark(item._id))
+										}}
+										className='text-gray-500 hover:text-white'
+									>
+										<MdDeleteForever className='w-6 h-6' />
+									</button>
+								</TableCell>
 							</TableRow>
-						</TableHeader>
-						<TableBody>
-							{visibleItems.map(item => (
-								<TableRow
-									key={item._id}
-									className='hover:bg-transparent cursor-pointer'
-									onClick={() => handleRowClick(item.coordinates)}
-								>
-									<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
-										{item.type}
-									</TableCell>
-									<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
-										{item.address}
-									</TableCell>
-									<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
-										{item.label}
-									</TableCell>
-									<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
-										{item.description}
-									</TableCell>
-									<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
-										{item.coordinates
-											? `${item.coordinates.latitude}, ${item.coordinates.longitude}`
-											: 'Не указаны'}
-									</TableCell>
-									<TableCell className='bg-gray-600 whitespace-nowrap border-black text-center'>
-										<button
-											onClick={e => {
-												e.stopPropagation()
-												dispatch(deleteMark(item._id))
-											}}
-											className='text-gray-500 hover:text-white'
-										>
-											<MdDeleteForever className='w-6 h-6' />
-										</button>
-									</TableCell>
-								</TableRow>
+						))}
+					</TableBody>
+				</Table>
+				{totalPages > 1 && (
+					<Pagination className='mt-4'>
+						<PaginationPrevious onClick={handlePreviousPage} />
+						<PaginationContent>
+							{Array.from({ length: totalPages }, (_, index) => (
+								<PaginationItem key={index}>
+									<PaginationLink
+										isActive={currentPage === index + 1}
+										onClick={() => setCurrentPage(index + 1)}
+									>
+										{index + 1}
+									</PaginationLink>
+								</PaginationItem>
 							))}
-						</TableBody>
-					</Table>
-					{totalPages > 1 && (
-						<Pagination className='mt-4'>
-							<PaginationPrevious onClick={handlePreviousPage} />
-							<PaginationContent>
-								{Array.from({ length: totalPages }, (_, index) => (
-									<PaginationItem key={index}>
-										<PaginationLink
-											isActive={currentPage === index + 1}
-											onClick={() => setCurrentPage(index + 1)}
-										>
-											{index + 1}
-										</PaginationLink>
-									</PaginationItem>
-								))}
-							</PaginationContent>
-							<PaginationNext onClick={handleNextPage} />
-						</Pagination>
-					)}
-				</div>
-			)}
+						</PaginationContent>
+						<PaginationNext onClick={handleNextPage} />
+					</Pagination>
+				)}
+			</div>
 		</>
 	)
 })
