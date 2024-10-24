eecd190f473121e245ac316bb3d30c230b161784
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-10-24 15:43:02 +0500

    feat:  addMark asyncAction added

--- src/redux/asyncActions/marksActions.ts
+++ src/redux/asyncActions/marksActions.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
-import { IMarker } from '@@/types/types'
+import { IMarker, IFormData } from '@@/types/types'
 
+// Получение меток с mongodb
 export const fetchMarks = createAsyncThunk<IMarker[]>(
 	'marks/fetchMarks',
 	async () => {
@@ -13,6 +14,7 @@ export const fetchMarks = createAsyncThunk<IMarker[]>(
 	}
 )
 
+// удаление метки
 export const deleteMark = createAsyncThunk(
 	'marks/deleteMark',
 	async (id: string, { rejectWithValue }) => {
@@ -24,6 +26,32 @@ export const deleteMark = createAsyncThunk(
 				throw new Error('Ошибка при удалении метки с сервера')
 			}
 			return id
+		} catch (error) {
+			if (error instanceof Error) {
+				return rejectWithValue(error.message)
+			}
+			return rejectWithValue('Произошла неизвестная ошибка')
+		}
+	}
+)
+
+// добавление метки
+export const addMark = createAsyncThunk(
+	'marks/addMark',
+	async (markerData: IFormData, { rejectWithValue }) => {
+		try {
+			const response = await fetch('/api/markers', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(markerData)
+			})
+
+			if (!response.ok) {
+				throw new Error('Ошибка при добавлении метки')
+			}
+
+			const data = await response.json()
+			return data.marker
 		} catch (error) {
 			if (error instanceof Error) {
 				return rejectWithValue(error.message)
